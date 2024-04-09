f803d0298cf6d607adf3a54b20a3d8837e1449cd
akshay6970 <149579502+akshay6970@users.noreply.github.com>
2024-04-09 10:49:00 +0530

    first-commit
    first-commit

--- admin/javascript/script.js
+++ admin/javascript/script.js
@@ -1,13 +1,49 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let toggle = document.querySelector(".toggle");
     let sideContainer = document.querySelector(".side-bar-Container");
     let title = document.querySelectorAll('.menu-title')
-    let index ;
+    let main = document.querySelector('.main');
+    let index;
 
-    toggle.addEventListener("click", function() {
+    toggle.addEventListener("click", function () {
         console.log('you clicked');
         sideContainer.classList.toggle("active");
+        main.classList.toggle("active");
         title.classList.toggle("none");
+
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let ctx = document.getElementById('myChart').getContext("2d");
+
+    let firstHalf =[2006, 2007, 2008 , 2009 , 2010]
+    let secondHalf =[0, 25, 50 , 75 , 100]
+
+    var neww = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["python", "Javascript", "php", "Java", "c##", "C++"],
+            datasets: [
+                {
+                data: secondHalf,
+                label:"false",
+            }
+        ],
+        },
+        options:{
+            tooltips:{
+                intersect : false,
+                mode:'index'
+                },
+            responsive:false,
+        },
+        legend:{
+            display:true,
+            position:"bottom",
+            align:"center",
+        }
 
     });
 });
